79fd199757c3ac13c3d7cca66ca20193a37c8469
Juampa14 <juanpabloquiroz45@gmail.com>
2025-10-05 16:19:09 -0400

    feat: desarrollo update-responsable.dto

--- src/responsables/dto/update-responsable.dto.ts
+++ src/responsables/dto/update-responsable.dto.ts
@@ -1,4 +1,3 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateResponsableDto } from './create-responsable.dto';
 
-export class UpdateResponsableDto extends PartialType(CreateResponsableDto) {}
