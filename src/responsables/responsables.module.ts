ee526be0608c7a5dd7bc421910d8bad5b1746dab
Juampa14 <juanpabloquiroz45@gmail.com>
2025-10-05 16:26:13 -0400

    feat: solucion a problema responsables.module.dto.ts

--- src/responsables/responsables.module.ts
+++ src/responsables/responsables.module.ts
@@ -3,5 +3,9 @@ import { ResponsablesService } from './responsables.service';
 import { ResponsablesController } from './responsables.controller';
 import { PrismaModule } from '../prisma/prisma.module';
 
-
+@Module({
+  imports: [PrismaModule],
+  controllers: [ResponsablesController],
+  providers: [ResponsablesService],
+})
 export class ResponsablesModule {}
