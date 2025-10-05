331e47d59da28d51e42757bc81eab86de59e0b1b
vivirome <vivianrochamedina@gmail.com>
2025-10-05 16:14:34 -0400

    feat: creacion archivo filter-responsable.dto

--- src/responsables/dto/filter-responsable.dto.ts
+++ src/responsables/dto/filter-responsable.dto.ts
@@ -1,11 +1,1 @@
-import { IsOptional, IsNumber, IsBoolean } from 'class-validator';
 
-export class FilterResponsableDto {
-  @IsOptional()
-  @IsNumber()
-  id_area?: number;
-
-  @IsOptional()
-  @IsBoolean()
-  activo?: boolean;
-}
