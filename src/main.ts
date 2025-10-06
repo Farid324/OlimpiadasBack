1409f1e8ce0112f3c9c5d4ea8b54e93a2b74c7d6
Sebastián Negrete <sebas.negrete.17.09@gmail.com>
2025-10-06 03:04:02 -0400

    chore: registrar globalmente BigIntSerializerInterceptor en main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { BigIntSerializerInterceptor } from './common/interceptors/bigint-serializer.interceptor'; // 👈 importa
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+  app.useGlobalInterceptors(new BigIntSerializerInterceptor());
   await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
