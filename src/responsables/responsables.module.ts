c27d15ce8425b713a81ccb0c8beafe123424beb6
Juampa14 <juanpabloquiroz45@gmail.com>
2025-10-06 17:23:13 -0400

    feat: correcciones generales de back

--- src/responsables/responsables.module.ts
+++ src/responsables/responsables.module.ts
@@ -3,9 +3,5 @@ import { ResponsablesService } from './responsables.service';
 import { ResponsablesController } from './responsables.controller';
 import { PrismaModule } from '../prisma/prisma.module';
 
-@Module({
-  imports: [PrismaModule],
-  controllers: [ResponsablesController],
-  providers: [ResponsablesService],
-})
+
 export class ResponsablesModule {}
