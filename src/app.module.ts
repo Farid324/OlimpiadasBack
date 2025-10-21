f6ebcd7d301af0c58dea8af37e7c1621e0dda76f
Juampa14 <juanpabloquiroz45@gmail.com>
2025-10-21 19:39:46 -0400

    feat: creacion de modulo para recuperar niveles

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,29 +1,23 @@
 // src/app.module.ts
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
+import { JwtModule } from '@nestjs/jwt';
 
 // Auth
 import { AuthController } from './auth/controllers/auth.controller';
 import { AuthService } from './auth/services/auth.service';
-import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './auth/strategies/jwt.strategy';
 
 // Users
 import { UsersController } from './users/users.controller';
 import { UsersService } from './users/users.service';
-import { AreasModule } from './areas/areas.module';
 
-// Nuevos módulos
+// Módulos
+import { AreasModule } from './areas/areas.module';
 import { ResponsablesModule } from './responsables/responsables.module';
 import { EvaluadoresModule } from './evaluadores/evaluadores.module';
-
-//Olimpistas
 import { OlimpistasModule } from './olimpistas/olimpistas.module';
-
-//Grupos Olimpistas
 import { GruposModule } from './grupos/grupos.module';
-
-//Tutores
 import { TutoresModule } from './tutores/tutores.module';
 
 @Module({
@@ -33,13 +27,12 @@ import { TutoresModule } from './tutores/tutores.module';
     OlimpistasModule,
     GruposModule,
     TutoresModule,
+    EvaluadoresModule,
+    ResponsablesModule,
     JwtModule.register({
       secret: process.env.JWT_SECRET,
       signOptions: { expiresIn: process.env.JWT_EXPIRES_IN || '1d' },
     }),
-    EvaluadoresModule,
-    ResponsablesModule, // 🔹 módulo de responsables
-    AreasModule, // 🔹 módulo de áreas
   ],
   controllers: [AuthController, UsersController],
   providers: [AuthService, JwtStrategy, UsersService],
