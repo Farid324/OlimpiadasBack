e6b327050a269cc48f3b8666ba72401de967c2f4
vivirome <vivianrochamedina@gmail.com>
2025-10-21 19:35:56 -0400

    feat: creacion de controlador para recuperar niveles

---
+++ src/niveles/niveles.controller.ts
@@ -0,0 +1,15 @@
+//src/niveles/niveles/niveles.controller.ts
+
+import { Controller, Get } from '@nestjs/common';
+import { NivelesService } from './niveles.service';
+
+@Controller('niveles')
+export class NivelesController {
+  constructor(private readonly nivelesService: NivelesService) {}
+
+  @Get()
+  async findAll() {
+    const list = await this.nivelesService.findAll();
+    return list.map(n => ({ id: n.id_nivel, nombre: n.nombre_nivel }));
+  }
+}
