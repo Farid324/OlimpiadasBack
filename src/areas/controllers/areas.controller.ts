306ebfde76333f2eeed55581bb1c28df170f832e
Farid <geelgaga@gmail.com>
2025-10-25 12:19:04 -0400

    chore(errores):modificacion y errores

--- src/areas/controllers/areas.controller.ts
+++ src/areas/controllers/areas.controller.ts
@@ -1,22 +1,20 @@
-// src/areas/controllers/areas.controller.ts
 import { Controller, Get, UseGuards } from '@nestjs/common';
 import { AreasService } from '../services/areas.service';
 import { JwtAuthGuard } from '../../common/guards/jwt-auth.guard';
-import { RolesGuard } from '../../common/guards/roles.guard';
-import { Roles } from '../../common/decorators/roles.decorator';
-import { ADMIN } from '../../auth/constants';
+// import { RolesGuard } from '../../common/guards/roles.guard'; // Ya no lo usamos aquí
+// import { Roles } from '../../common/decorators/roles.decorator'; // Ya no lo usamos aquí
+// import { ADMIN } from '../../auth/constants'; // Ya no lo usamos aquí
 
 @Controller('areas')
-@UseGuards(JwtAuthGuard, RolesGuard)
-@Roles(ADMIN)
+@UseGuards(JwtAuthGuard)
 export class AreasController {
   constructor(private readonly areasService: AreasService) {}
 
   @Get()
   async getAreas() {
-    console.log('💡 Llamada al Controller recibida');
+    console.log('PRUEBA DE REINICIO v2 - ESTE ES EL CÓDIGO NUEVO');
     const areas = await this.areasService.getAreasConEstadisticas();
-    console.log('💡 Datos devueltos por el servicio:', areas);
+    console.log('Datos devueltos por el servicio:', areas);
     return areas;
   }
 }
