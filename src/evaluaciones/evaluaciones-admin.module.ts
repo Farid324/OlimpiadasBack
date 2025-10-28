3fc175b8488cd195c51ddd0dc0e05b638443f53d
montmelina917 <montmelina917@gmail.com>
2025-10-28 08:36:46 -0400

    feat: Implementación del nuevo servicio para registrar y editar notas

--- src/evaluaciones/evaluaciones-admin.module.ts
+++ src/evaluaciones/evaluaciones-admin.module.ts
@@ -3,10 +3,11 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from '../prisma/prisma.module';
 import { EvaluacionesAdminController } from './evaluaciones.controller';
 import { EvaluacionesAdminService } from './evaluaciones.service';
+import { EvaluacionesService } from './registrar-editar.service';
 
 @Module({
   imports: [PrismaModule],
   controllers: [EvaluacionesAdminController],
-  providers: [EvaluacionesAdminService],
+  providers: [EvaluacionesAdminService, EvaluacionesService],
 })
 export class EvaluacionesAdminModule {}
