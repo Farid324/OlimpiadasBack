c1fcb9d4acb4ed1601c7257f0d46f74e26767884
montmelina917 <montmelina917@gmail.com>
2025-10-31 17:49:18 -0400

    fix: Recuperar Nicvel del competidor

--- src/evaluaciones/evaluaciones.service.ts
+++ src/evaluaciones/evaluaciones.service.ts
@@ -40,6 +40,13 @@ export class EvaluacionesAdminService {
             escuela: true,
           },
         },
+        nivel: {
+          select: {
+            id_nivel: true,
+            nombre_nivel: true,
+          },
+        },
+        clasificacion: true,
         evaluaciones: {
           //where: { id_fase: 1 }, // si necesitas filtrar por fase
           orderBy: { fecha_registro: 'desc' }, // la mas reciente primero
